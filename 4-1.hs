449d771f42f6d0c6afa9397cbf16e9fb8b9d63a3
shigemk2 <i.am.shige@gmail.com>
2014-11-01 21:51:23 +0900

    maximum' debug

--- 4-1.hs
+++ 4-1.hs
@@ -1,6 +1,9 @@
+import Debug.Trace
 maximum' [] = error "maximum of empty list!"
 maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
+maximum' (x:xs) = trace dbg1 $ max x (maximum' xs)
+    where
+      dbg1 = show x ++ " " ++ show xs
 
 main = do
-  print $ maximum' [1,2,3,4,5]
+  print $ maximum' [1,6,3]
